c4bff18c94bd16c3f54f9f4a48ae3d3e8b21e353
Ed <edhillmann@yahoo.com>
2014-10-15 22:42:27 +1000

    Problem 21: optimized

--- src/main/scala/com/csea/projecteuler/Problem021.scala
+++ src/main/scala/com/csea/projecteuler/Problem021.scala
@@ -7,9 +7,39 @@
 package com.csea.projecteuler
 
 object Problem021 {
+  def oldDivisors(n: Int) : List[Int] = for (i <- List.range(1, n-1) if n % i == 0) yield i
+  
+  def divisors1(n: Int): List[Int] = {
+      var retVal = List(1)
+      val largest = upperBound(n)
+      for (i <- Range(2, largest)) {
+        if (n % i == 0) {
+          retVal = retVal :+ i
+          retVal = retVal :+ (n / i)
+        }
+      }
+      retVal
+    }
 
-  def divisors(n: Int): List[Int] = 
-    for (i <- List.range(1, n-1) if n % i == 0) yield i
+    def divisors(n: Int): List[Int] = {
+      List(1) ::: Range(2,upperBound(n)).flatMap(i => if (isDivisorOf(n, i)) { List(i, n/i)} else {List()} ).toList
+    }
+    
+    def isDivisorOf(n:Int, div: Int): Boolean = {
+      div match {
+        case 1 => true
+        case _ => n % div == 0
+      }
+    }
+
+  def upperBound(n: Int) = {
+    val largest = scala.math.sqrt(n).intValue
+    if ((largest * largest) != n) {
+      largest + 1
+    } else {
+      largest
+    }
+  }
   
   def amicableNumber(a: Int) = {
     val b = divisors(a).sum //d(a) = b
@@ -21,6 +51,7 @@ object Problem021 {
    * @param args the command line arguments
    */
   def main(args: Array[String]): Unit = {
+    // should be 31626
     println((for (i <- List.range(1, 9999) if amicableNumber(i)) yield i).sum)
   }
 
