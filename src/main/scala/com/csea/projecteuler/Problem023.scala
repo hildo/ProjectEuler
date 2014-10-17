bf40c45fee467ccc7845eb4c63863aece96604ce
Ed <edhillmann@yahoo.com>
2014-10-17 22:54:29 +1000

    Problem 23: solved, although I wish my divisor function was smaller

--- src/main/scala/com/csea/projecteuler/Problem023.scala
+++ src/main/scala/com/csea/projecteuler/Problem023.scala
@@ -8,32 +8,34 @@ package com.csea.projecteuler
 
 object Problem023 {
 
-  // Can this be optimised (see http://codereview.stackexchange.com/questions/39946/optimizing-solution-for-project-euler-problem-23-non-abundant-sums)
-  def divisors(n: Int): List[Int] = for (i <- List.range(1, n-1) if n % i == 0) yield i
-
+    def divisors(n: Int): List[Int] = {
+      var retVal = List(1)
+      if (n > 2) {
+        var largest = scala.math.sqrt(n).intValue
+        if (largest * largest == n) {
+          retVal = retVal :+ largest
+        } else {
+          largest = largest + 1
+        }
+        for (i <- 2 to largest) {
+          if (n % i == 0) {
+            retVal = retVal :+ i
+            retVal = retVal :+ (n / i)
+          }
+        }
+      }
+      retVal.distinct
+    }
+    
   def isAbundant(n: Int): Boolean = divisors(n).sum > n
 
   val abundantNumbers = for (i <- List.range(1, 28123) if isAbundant(i)) yield i
 
-  // This is running too long
   def isSumOfTwoAbundantNumbers(n: Int): Boolean = {
-    // Find the abundant numbers that are less than n.
-    // See if they can be used to 
-    println("Checking " + n)
     if (n < 24) {
       false
     } else {
-      var index = 0
-      var abNum = abundantNumbers(index)
-      var keepLooking = true
-      while (keepLooking && (abNum < n)) {
-        keepLooking = !isAbundant(n - abNum)
-        if (keepLooking) {
-          index = index + 1
-          abNum = abundantNumbers(index)
-        }
-      }
-      !keepLooking
+      !abundantNumbers.filter(x => x < n).find(y => isAbundant(n-y)).isEmpty
     }
   }
   
@@ -42,8 +44,7 @@ object Problem023 {
    */
   def main(args: Array[String]): Unit = {
     val fromNonAbundantSums = for (i <- List.range(1, 28123) if !isSumOfTwoAbundantNumbers(i)) yield i
-    // http://www.mathblog.dk/project-euler-23-find-positive-integers-not-sum-of-abundant-numbers/
-    println(fromNonAbundantSums.toString)
+    println(fromNonAbundantSums.sum)
   }
 
 }
