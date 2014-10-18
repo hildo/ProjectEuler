54e4d5b91439621d0b072ba64b5574e0316e749c
Ed Hillmann <edhillmann@yahoo.com>
2014-10-18 14:03:07 +1000

    Problem 25 solved

--- src/main/scala/com/csea/projecteuler/Problem025.scala
+++ src/main/scala/com/csea/projecteuler/Problem025.scala
@@ -5,8 +5,15 @@
 
 package com.csea.projecteuler
 
+import scala.math.BigInt
+
 object Problem025 {
 
-  lazy val fib: Stream[Int] = Stream.cons(0, Stream.cons(1, fib.zip(fib.tail) map { case (c1,c2) => c1 + c2 } ))
+  val fibs: Stream[BigInt] = BigInt(1) #:: BigInt(1) #:: fibs.zip(fibs.tail).map(n => n._1 + n._2)
+
+  def main(args: Array[String]): Unit = {
+    println(fibs.indexWhere(n => n.toString.length == 1000) + 1)
+    // Index is 4781, but the index is zero based, so answer is 4782
+  }
 
 }
